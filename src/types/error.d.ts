771869e3c4282ebfceedbf9e483122b9aed5d236
ryonakaya <nakayaryo0716@gmail.com>
2024-10-20 11:20:55 +0900

    [fix]エラーメッセージの定義を/src/util/errorHandle.tsに移動

--- src/types/error.d.ts
+++ src/types/error.d.ts
@@ -1,16 +1,3 @@
 export type ErrorResMsg = {
     error: string,
 }
-
-// ユーザー情報に関するエラー
-export const userAlreadyExists = "User already exists";
-export const userNotFound = "User not found";
-//ルームに関するエラー
-export const roomNotFound = "Room not found";
-// 認証に関するエラー
-export const missingCredentials = "Missing credentials";
-export const wrongCredentials = "Wrong credentials";
-export const tokenCreation = "Token creation error";
-export const invalidToken = "Invalid token";
-// その他のエラー
-export const unexpected = "Server error occurred";
